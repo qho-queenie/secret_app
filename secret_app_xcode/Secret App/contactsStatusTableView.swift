1bda350b05a3be701f4bc1eefda8ed84eb965619
Queenie Ho <qho.queenieho@gmail.com>
2017-02-17 14:08:22 -0800

    added edit profile buttons

---
+++ secret_app_xcode/Secret App/contactsStatusTableView.swift
@@ -0,0 +1,27 @@
+//
+//  contactsStatusTableView.swift
+//  Secret App
+//
+//  Created by Queenie Ho on 2/17/17.
+//  Copyright © 2017 SecretApp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+class contactsStatusTableView: UITableViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Set Time View Controller")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
---
+++ secret_app_xcode/Secret App/editProfile.swift
@@ -0,0 +1,15 @@
+//
+//  editProfile.swift
+//  Secret App
+//
+//  Created by Queenie Ho on 2/17/17.
+//  Copyright © 2017 SecretApp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class editProfile : UIButton{
+    
+}
+
