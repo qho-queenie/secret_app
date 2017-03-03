31c07bdac8f6728282b38b8b41936dbfee5865b3
Chris Rollins <chris.rollins@leapfire.com>
2017-03-02 20:40:46 -0800

    reload when comes back from background

--- secret_app_xcode/Secret App/AppDelegate.swift
+++ secret_app_xcode/Secret App/AppDelegate.swift
@@ -36,7 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        print(self.window?.rootViewController?.viewWillAppear(false))
+        self.window?.rootViewController?.viewDidLoad()
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
